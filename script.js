4ea574c89ee885cc3fbe074932c7fdf05f747a5f
Siva Prasad <sivaprasad.ummaneni@gmail.com>
2025-02-20 15:34:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// GSAP animations
+gsap.to(".cover", { opacity: 1, y:-10, duration: 1, delay:0.5});
+gsap.to(".profile", { opacity: 1, y: -10, duration: 1, delay: 0.5 });
+gsap.to("header", { opacity: 1, y: -10, duration: 1, delay: 1.5 });
+gsap.to("#about", { opacity: 1, y: -10, duration: 1, delay: 1.5 });
